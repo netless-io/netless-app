eaa7a7789527fb57a9fe2c20052e60dd979979d6
hyrious <hyrious@outlook.com>
2021-09-14 11:07:19 +0800

    fix(playground): base path

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,6 +2,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "",
   plugins: [
     svelte({
       emitCss: false,
