f55f00fa41d5e0a51e22895f1081a6f4da18c564
crimx <straybugs@gmail.com>
2021-09-17 18:55:30 +0800

    feat(shared): add ensure-attributes

---
+++ packages/app-shared/src/ensure-attributes.ts
@@ -0,0 +1,24 @@
+import type { AppContext } from "@netless/window-manager";
+
+function isObject(val: unknown): val is Record<string, unknown> {
+  return val != null && typeof val === "object" && !Array.isArray(val);
+}
+
+export function ensureAttributes<T>(context: AppContext<T>, initAttrs: T): T {
+  let attrs = context.getAttributes();
+  if (!attrs) {
+    context.setAttributes(initAttrs);
+    attrs = context.getAttributes();
+  }
+  if (!attrs) {
+    throw new Error("[NetlessAppMonaco] No attributes");
+  }
+  if (isObject(initAttrs)) {
+    Object.keys(initAttrs).forEach(key => {
+      if (!Object.prototype.hasOwnProperty.call(attrs, key)) {
+        context.updateAttributes([key], initAttrs[key]);
+      }
+    });
+  }
+  return attrs;
+}
--- packages/app-shared/src/index.ts
+++ packages/app-shared/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./svelte";
+export * from "./ensure-attributes";
