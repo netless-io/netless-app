61d00ec942dd07250aad4d41a600d8f0ae29173c
crimx <straybugs@gmail.com>
2021-11-04 16:58:07 +0800

    refactor(shared): update logger log level

--- packages/app-shared/src/logger.ts
+++ packages/app-shared/src/logger.ts
@@ -1,25 +1,34 @@
 import randomColor from "randomcolor";
 
+export type LoggerDebugLevel = boolean | "log" | "warn" | "error";
+
 export class Logger {
   private color = randomColor({ luminosity: "dark" });
 
-  public constructor(public kind = "NetlessApp", public debug = import.meta.env.DEV) {}
+  public constructor(
+    public kind = "NetlessApp",
+    public debug: LoggerDebugLevel = import.meta.env.DEV || "error"
+  ) {}
 
   public log(...messages: unknown[]): void {
-    return this._log("log", messages);
+    if (this.debug === true || this.debug === "log") {
+      return this._log("log", messages);
+    }
   }
 
   public warn(...messages: unknown[]): void {
-    return this._log("warn", messages);
+    if (this.debug && this.debug !== "error") {
+      return this._log("warn", messages);
+    }
   }
 
   public error(...messages: unknown[]): void {
-    return this._log("error", messages);
+    if (this.debug) {
+      return this._log("error", messages);
+    }
   }
 
   private _log(type: "log" | "warn" | "error", messages: unknown[]): void {
-    if (this.debug) {
-      console[type](`%c[${this.kind}]:`, `color: ${this.color}; font-weight: bold;`, ...messages);
-    }
+    console[type](`%c[${this.kind}]:`, `color: ${this.color}; font-weight: bold;`, ...messages);
   }
 }
