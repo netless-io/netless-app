033be898c302c338a1fdb4df9ae1a8bf3c10bffc
CRIMX <straybugs@gmail.com>
2021-12-16 15:48:02 +0800

    refactor(monaco): update tio encoding

--- packages/app-monaco/src/compiler/tio.ts
+++ packages/app-monaco/src/compiler/tio.ts
@@ -18,22 +18,39 @@ export class Tio implements Compiler {
     try {
       const l = unescape(encodeURIComponent(Tio.lanMap[lang]));
       const c = unescape(encodeURIComponent(source));
-      const plain = `Vlang\0${1}\0${l}\0VTIO_OPTIONS\0${0}\0F.code.tio\0${
+      const plain = `Vlang\0${1}\0${l}\0VTIO_CFLAGS\0${0}\0F.code.tio\0${
         c.length
       }\0${c}F.input.tio\0${0}\0Vargs\0${0}\0R`;
 
       const response = await fetch("https://tio.run/cgi-bin/run/api/", {
         method: "POST",
-        body: new Blob([deflateRaw(plain)]),
+        body: new Blob([deflateRaw(encodeText(plain), { level: 9 })]),
       });
 
       if (!response.ok) {
         return `${response.status} ${response.statusText}`;
       }
 
-      const lines = (await response.text()).split("\n");
-      const result = lines.slice(0, lines.length - 5).join("\n");
-      const splitter = result.slice(0, 16);
+      const rawText = await response.text();
+      const splitter = rawText.slice(0, 16);
+      const lines = rawText.split("\n");
+
+      if (!rawText.includes("Real time:")) {
+        return rawText.split(splitter).join("");
+      }
+
+      for (let i = lines.length - 1; i >= 0; i--) {
+        const line = lines[i];
+        if (line.startsWith(splitter)) {
+          break;
+        }
+        lines.pop();
+        if (line.startsWith("Real time:")) {
+          break;
+        }
+      }
+
+      const result = lines.join("\n");
 
       const [out, err] = result.slice(16).split(splitter);
 
@@ -75,4 +92,12 @@ export class Tio implements Compiler {
     typescript: "typescript",
     vb: "vb-core",
   };
+}
+
+function encodeText(text: string): Uint8Array {
+  const byteArray = new Uint8Array(text.length);
+  for (let index = 0; index < text.length; index++) {
+    byteArray[index] = text.charCodeAt(index);
+  }
+  return byteArray;
 }
