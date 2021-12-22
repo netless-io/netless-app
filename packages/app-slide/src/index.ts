e37aeaa7508f8aa4dd556605695ab8ba13871111
hyrious <hyrious@outlook.com>
2021-12-22 14:59:49 +0800

    chore(slide): upgrade deps and bump version

--- packages/app-slide/src/index.ts
+++ packages/app-slide/src/index.ts
@@ -23,7 +23,7 @@ export { SlidePreviewer, default as previewSlide } from "./SlidePreviewer";
 
 export type { Attributes, AddHooks, FreezableSlide };
 
-export const version = "0.0.51";
+export const version = "0.0.52";
 
 export { DefaultUrl, apps, FreezerLength, addHooks };
 
