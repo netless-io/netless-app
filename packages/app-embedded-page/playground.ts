045dfddac77149ef6bf98686b6c28b22543f144d
CRIMX <straybugs@gmail.com>
2022-02-24 10:55:25 +0800

    refactor(embedded-page): add google docs

--- packages/app-embedded-page/playground.ts
+++ packages/app-embedded-page/playground.ts
@@ -2,10 +2,14 @@ import type { PlaygroundConfigs } from "../playground/typings";
 import type { Attributes } from "./src";
 
 const demo: { title: string; src: string }[] = [
+  // {
+  //   title: "Scratch",
+  //   src: "http://192.168.31.195:8601/",
+  //   // scenePath: `/embedded-page/${title}`,
+  // },
   {
-    title: "Scratch",
-    src: "http://192.168.31.195:8601/",
-    // scenePath: `/embedded-page/${title}`,
+    title: "Google Docs",
+    src: "https://docs.google.com/document/d/1bd4SRb5BmTUjPGrFxU2V7KI2g_mQ-HQUBxKTxsEn5e4/edit?usp=sharing",
   },
 ];
 
