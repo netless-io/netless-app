12986c8cea99ffedc72a0fccccbe366084b4af8b
hyrious <hyrious@outlook.com>
2022-06-22 15:22:44 +0800

    refactor(slide): upgrade window manager (#14)

--- packages/app-slide/playground.ts
+++ packages/app-slide/playground.ts
@@ -1,19 +1,26 @@
 import type { PlaygroundConfig, PlaygroundConfigs } from "../playground/typings";
 import type { Attributes } from "./src";
-import { addHooks } from "./src/utils/freezer";
+import { addHooks } from "./src";
 
-function definePPT(title: string, taskId: string, url?: string): PlaygroundConfig<Attributes> {
+function definePPT(
+  title: string,
+  taskId: string,
+  scenePath?: string,
+  url?: string
+): PlaygroundConfig<Attributes> {
   return {
     kind: "Slide",
     src: () => import("./src"),
-    options: { title, scenePath: `/Slide/${taskId}` },
+    options: { title, scenePath: scenePath || `/Slide/${taskId}` },
     attributes: { taskId, url },
     addHooks,
   };
 }
 
 const options: PlaygroundConfigs<Attributes> = [
-  definePPT("星空", "9abed6605bbc11ec88a83b917638a00c"),
+  definePPT("星空1", "9abed6605bbc11ec88a83b917638a00c", "/Slide/1"),
+  definePPT("星空2", "9abed6605bbc11ec88a83b917638a00c", "/Slide/2"),
+  definePPT("星空3", "9abed6605bbc11ec88a83b917638a00c", "/Slide/3"),
 ];
 
 export default options;
