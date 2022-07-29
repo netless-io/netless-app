8f9001af8d2ff0f4248b4dce8a6a51b549e3fb24
l1shen <648952316@qq.com>
2022-07-29 14:28:30 +0800

    fix: flowchart sync (#16)

--- packages/app-flowchart/src/flow/flow-sync.ts
+++ packages/app-flowchart/src/flow/flow-sync.ts
@@ -1,8 +1,8 @@
-import { XFlowNodeCommands, XFlowEdgeCommands, XFlowGroupCommands, MODELS } from "@antv/xflow";
+import { XFlowNodeCommands, MODELS, XFlowGraphCommands } from "@antv/xflow";
 import { SideEffectManager } from "side-effect-manager";
 import type { NsGraph, IApplication } from "@antv/xflow";
 import type { Storage } from "@netless/window-manager";
-import { isEqual, omit } from "lodash-es";
+import { isEqual } from "lodash-es";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyFunc = (...args: any[]) => any;
@@ -53,62 +53,8 @@ export class FlowSync {
     this._sideEffect.add(() => selectNodeDispose.dispose);
   }
 
-  public addNode: AnyFunc = async (nodeConfig: NsGraph.INodeConfig) => {
-    const node = await this.app?.getNodeById(nodeConfig.id);
-    if (node) return;
-    return this.app?.executeCommand(XFlowNodeCommands.ADD_NODE.id, { nodeConfig });
-  };
-
-  public updateNode: AnyFunc = async (nodeConfig: NsGraph.INodeConfig) => {
-    const node = await this.app?.getNodeById(nodeConfig.id);
-    const nextConfig = omit(nodeConfig, ["isCollapsed"]);
-    if (node?.toJSON() === nextConfig) return;
-    return this.app?.executeCommand(XFlowNodeCommands.UPDATE_NODE.id, { nodeConfig: nextConfig });
-  };
-
-  public delNode: AnyFunc = async (id: string) => {
-    return this.app?.executeCommand(XFlowNodeCommands.DEL_NODE.id, { nodeConfig: { id } });
-  };
-
-  public addEdge: AnyFunc = async (edgeConfig: NsGraph.IEdgeConfig) => {
-    const edge = await this.app?.getEdgeById(edgeConfig.id);
-    if (edge) return;
-    return this.app?.executeCommand(XFlowEdgeCommands.ADD_EDGE.id, { edgeConfig });
-  };
-
-  public updateEdge: AnyFunc = async (edgeConfig: NsGraph.IEdgeConfig) => {
-    const edge = await this.app?.getEdgeById(edgeConfig.id);
-    if (edge?.toJSON() === edgeConfig) return;
-    return this.app?.executeCommand(XFlowEdgeCommands.UPDATE_EDGE.id, { edgeConfig });
-  };
-
-  public delEdge: AnyFunc = async (edgeConfig: NsGraph.IEdgeConfig) => {
-    const edge = await this.app?.getEdgeById(edgeConfig.id);
-    if (!edge) return;
-    return this.app?.executeCommand(XFlowEdgeCommands.DEL_EDGE.id, { edgeConfig });
-  };
-
-  public addGroup: AnyFunc = async (groupConfig: NsGraph.IGroupConfig) => {
-    const group = await this.app?.getNodeById(groupConfig.id);
-    if (group) return;
-    return this.app?.executeCommand(XFlowGroupCommands.ADD_GROUP.id, { nodeConfig: groupConfig });
-  };
-
-  public updateGroup: AnyFunc = async (groupConfig: NsGraph.IGroupConfig) => {
-    const group = await this.app?.getNodeById(groupConfig.id);
-    if (isEqual(group?.toJSON(), groupConfig)) return;
-    return this.app?.executeCommand(XFlowNodeCommands.UPDATE_NODE.id, { nodeConfig: groupConfig });
-  };
-
-  public delGroup: AnyFunc = async (id: string) => {
-    return this.app?.executeCommand(XFlowGroupCommands.DEL_GROUP.id, { nodeConfig: { id } });
-  };
-
-  public collapseGroup: AnyFunc = async (id: string, isCollapsed: boolean) => {
-    return this.app?.executeCommand(XFlowGroupCommands.COLLAPSE_GROUP.id, {
-      nodeId: id,
-      isCollapsed,
-    });
+  public updateGraph: AnyFunc = async (graphData: any) => {
+    return this.app?.executeCommand(XFlowGraphCommands.GRAPH_RENDER.id, { graphData });
   };
 
   public dispose() {
