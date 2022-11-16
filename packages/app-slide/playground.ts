32cf0c57abb9d388169645bcb69332ce212a424a
hyrious <hyrious@outlook.com>
2022-11-16 10:47:46 +0800

    refactor(slide): add 2 demo slides

--- packages/app-slide/playground.ts
+++ packages/app-slide/playground.ts
@@ -18,9 +18,9 @@ function definePPT(
 }
 
 const options: PlaygroundConfigs<Attributes> = [
-  definePPT("星空1", "9abed6605bbc11ec88a83b917638a00c", "/Slide/1"),
-  definePPT("星空2", "9abed6605bbc11ec88a83b917638a00c", "/Slide/2"),
-  definePPT("星空3", "9abed6605bbc11ec88a83b917638a00c", "/Slide/3"),
+  definePPT("星空", "9abed6605bbc11ec88a83b917638a00c"),
+  definePPT("Flat (English)", "5f10c72cd0eb446282b99c3bab14ee93"),
+  definePPT("Flat (中文)", "7904da9ed7424ede961e9873fac60c64"),
 ];
 
 export default options;
